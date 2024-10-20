e9fad14e49bb031103712412c1fbaaab18b88e6b
Vikas <101702902+NotYourEren@users.noreply.github.com>
2024-10-20 23:07:22 +0530

    changes to server.js

--- backend/server.js
+++ backend/server.js
@@ -4,4 +4,39 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 
 const app = express();
-const port = process.env.PORT || 5000;
+const Port = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGO_URI;
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    methods: ["GET, POST, PUT, DELETE"],
+    allowedHeaders: ["Content-Type, Authorization"],
+  })
+);
+// Middleware
+app.use(express.json());
+
+//database connection
+mongoose
+  .connect(MONGO_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log("Error connecting to MongoDB", err);
+  });
+
+//routes configuration
+
+app.use((err, req, res, next) => {
+  console.log(err.stack);
+  res.status(500).json({
+    message: "Something went wrong",
+    success: false,
+  });
+});
+
+app.listen(Port, () => {
+  console.log(`Server is now running on port ${Port}`);
+});
